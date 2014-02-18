ff55864438529bc6eb7faa40aac2f213e8539383
flappycoin <admin@flappycoin.info>
2014-02-18 19:43:14 +0700

    patch not syncing wallet

--- src/net.cpp
+++ src/net.cpp
@@ -1192,7 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"dnsseed.flappycoin.info", "flappycoinpool.com"},
+    {"dnsseed.flappycoin.info", "dnsseed.flappycoinpool.com"},
     {NULL, NULL}
 };
 
